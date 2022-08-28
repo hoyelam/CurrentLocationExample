79302eb3e2134c982fb40ae0008b692e2cfbcd4c
Hoye Lam <hoyelam@protonmail.com>
2022-08-28 14:04:02 +0200

    Initial Commit

---
+++ CurrentLocationExample/CurrentLocationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentLocationExampleApp.swift
+//  CurrentLocationExample
+//
+//  Created by Hoye Lam on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrentLocationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
